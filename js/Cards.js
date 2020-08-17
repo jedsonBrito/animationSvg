5f25b078b69315bb9b5cc603db881137efcc653d
jedsonBrito <jedsoncid@gmail.com>
2020-08-17 20:04:15 -0300

    Ajutando nome da classe do card direito

--- js/Cards.js
+++ js/Cards.js
@@ -7,7 +7,7 @@ class Cards{
   calculoScroll(){
     const posicao = document.getElementById('section-dicas').getBoundingClientRect()['y']
     console.log(posicao)
-    if(posicao <= 280){
+    if(posicao <= 300){
       document.querySelector('.card-esquerdo').style.backgroundColor = "blue";
       document.querySelector('.card-direito').style.backgroundColor = "red";
     } else {
