adb0effe252e8efabe3459856317ef247af0f0b7
jedsonBrito <jedsoncid@gmail.com>
2020-08-17 19:46:46 -0300

    Testando posicao do Card js

--- js/Cards.js
+++ js/Cards.js
@@ -6,8 +6,14 @@ class Cards{
 
   calculoScroll(){
     const posicao = document.getElementById('section-dicas').getBoundingClientRect()['y']
-
     console.log(posicao)
+    if(posicao <= 280){
+      document.querySelect('.card-esquerdo').backgroundColor = "blue";
+      document.querySelect('.card-direto').backgroundColor = "red";
+    } else {
+      document.querySelect('.card-esquerdo').backgroundColor = "white";
+      document.querySelect('.card-direto').backgroundColor = "white";
+    }
   }
 
 }
