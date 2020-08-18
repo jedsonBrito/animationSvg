aae618963323864903c9b593e5fd3fb9ec993586
jedsonBrito <jedsoncid@gmail.com>
2020-08-18 10:42:00 -0300

    incluindo chamada de funcao no script.js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ import { Icones } from './Icones.js'
 window.onload = () => {
   const animaCards = new Cards();
   document.addEventListener('scroll', animaCards.scrollCards.bind(animaCards))
+
+  const icones = new Icones();
+  icones.animaIcones();
 }
