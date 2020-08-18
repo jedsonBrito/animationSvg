2306376e45b0e0838b7430c16235fba0b00fad2a
jedsonBrito <jedsoncid@gmail.com>
2020-08-18 10:49:16 -0300

    verificando conteudo lista icones

--- js/Icones.js
+++ js/Icones.js
@@ -4,8 +4,10 @@ class Icones {
     }
 
     animaIcones() {
+      console.log(this.listaIcones)
         TweenMax.to(this.listaIcones, 0.5, {scale: 0.95, repeat: -1, yoyo: true})
     }
 }
 
 export { Icones }
+
